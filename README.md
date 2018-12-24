c402bf3d5a5d288dfb839916a49094ea33dde740
Jaco Malan <jacomalan.12@gmail.com>
2018-12-24 12:43:53 +0200

    Added LICENSE and README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fitch
+Fitch is a game I am developing as a side-project.
+Currently I am busy porting Fitch to Java since the C# version doesn't work on Linux.
+
+# Building
+
+## Linux / MacOS
+
+* Run the gradle task 'build'. E.g. `$ ./gradlew build`
+* To run the program, do the gradle task 'run'.
+
+## Windows
+
+* Run the gradle task 'build'. E.g. `gradlew.bat build`
+* To run the program, do the gradle task 'run'.
+
--- src/main/java/com/codelog/fitch/Main.java
+++ src/main/java/com/codelog/fitch/Main.java
@@ -1,3 +1,23 @@
+/*
+
+A platformer game written using OpenGL.
+    Copyright (C) 2017-2018  Jaco Malan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 package com.codelog.fitch;
 
 import com.codelog.fitch.game.Player;
@@ -8,7 +28,6 @@ import com.jogamp.newt.event.WindowAdapter;
 import com.jogamp.newt.event.WindowEvent;
 import com.jogamp.newt.opengl.GLWindow;
 import com.jogamp.opengl.*;
-import com.jogamp.opengl.math.Matrix4;
 import com.jogamp.opengl.util.Animator;
 import glm_.vec2.Vec2;
 
